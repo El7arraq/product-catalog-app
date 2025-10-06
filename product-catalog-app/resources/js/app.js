bdfffc6f5ce9c6f8b5ac51f90dd880eee1e885eb
El7arraq <el7arraq98@gmail.com>
2025-10-06 14:57:56 +0100

    add vue components for products listing

--- product-catalog-app/resources/js/app.js
+++ product-catalog-app/resources/js/app.js
@@ -1,1 +1,11 @@
+
 require('./bootstrap');
+
+import { createApp } from 'vue';
+import ProductList from './components/ProductList.vue';
+import ProductForm from './components/ProductForm.vue';
+
+const app = createApp({});
+app.component('product-list', ProductList);
+app.component('product-form', ProductForm);
+app.mount('#app');
